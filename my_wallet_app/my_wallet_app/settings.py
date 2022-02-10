3b6648621b63ad4c29690ef14bbb56279a35307a
NabeelKM <nabeelkahlil403@gmail.com>
2022-02-10 17:06:39 +0700

    remove django secret key

--- my_wallet_app/my_wallet_app/settings.py
+++ my_wallet_app/my_wallet_app/settings.py
@@ -9,18 +9,20 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x7+=#ip89dgkx&t45az!dq(qmihy$1$m+q4+im4xbxrrfsqf@w'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
